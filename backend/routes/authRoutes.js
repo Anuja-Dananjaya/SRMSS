c91d24868c15c61b088f8fa7d82a853dba841e7f
Anuja Dananjaya <anujastud23@gmail.com>
2026-05-28 12:57:00 +0530

    add auth module -login, register, getMe, JWT middleware, role check

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,3 +1,15 @@
 const express = require ('express');
 const router =  express.Router();
+const { register , login , getMe } = require ('../controllers/authController');
+const verifyToken = require ('../middleware/auth');
+const allowRoles = require('../middleware/roleCheck');
+
+// Public routes 
+router.post('/login', login);
+
+//Protected routes 
+router.post('/register', verifyToken, allowRoles('admin'), register);
+router.get ('/me', verifyToken, getMe);
+
 module.exports = router;
+
